249cfacf6a2675946012cfe683d280ef478f85ba
HackerDad2022 <opportunitiesintech4life@gmail.com>
2022-05-06 21:16:22 +0200

    09-Manage-State-With-Reducer-Hook

--- README.md
+++ README.md
@@ -37,3 +37,10 @@ Amazon Clone
 3. use state hook
 4. use effect hook
 5. use reducer hook
+
+09-Manage-State-With-Reducer-Hook
+
+1. replace useState to define reducer
+2. update fetch data
+   . using http methods
+3. get state from useReducer using switch statements
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,39 +1,68 @@
-import { Link } from 'react-router-dom';
-import {useEffect, useState} from 'react';
-import axios from 'axios'
+import { Link } from "react-router-dom";
+import { useEffect, useReducer } from "react";
+import logger from "use-reducer-logger";
+import axios from "axios";
 //import data from '../data';
 function HomeScreen() {
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case "FETCH_REQUEST":
+        return { ...state, loading: true };
+      case "FETCH_SUCCESS":
+        return { ...state, products: action.payload, loading: false };
+      case "FETCH_FAIL":
+        return { ...state, laoding: false, error: action.payload };
+      default:
+        return state;
+    }
+  };
 
   // fetching data
-  const [products, setProducts] = useState([]);
-  useEffect(() =>{
+  //const [products, setProducts] = useState([]);
+  const [{ loading, error, products }, dispatch] = useReducer(logger(reducer), {
+    products: [], //products array
+    loading: true,
+    error: "",
+  });
+  useEffect(() => {
     const fetchData = async () => {
-      const result = await axios.get('/api/products');
-      setProducts(result.data);
+      dispatch({ type: "FETCH_REQUEST" });
+      try {
+        const result = await axios.get("/api/products");
+        dispatch({ type: "FETCH_SUCCESS", payload: result.data });
+      } catch (err) {
+        dispatch({ type: "FETCH_FAIL", payload: err.message });
+      }
     };
     fetchData();
-  },[]);
+  }, []);
   // fetching data
   return (
     <div>
       <h1>Featured Products</h1>
       <div className="products">
-        {products.map((product) => (
-          <div className="product" key={product.slug}>
-            <Link to={`/product/${product.slug}`}>
-              <img src={product.image} alt={product.name} />
-            </Link>
-            <div className="product-info">
+        {loading ? (
+          <div>Loading...</div>
+        ) : error ? (
+          <div>{error}</div>
+        ) : (
+          products.map((product) => (
+            <div className="product" key={product.slug}>
               <Link to={`/product/${product.slug}`}>
-                <p>{product.name}</p>
+                <img src={product.image} alt={product.name} />
               </Link>
-              <p>
-                <strong>${product.price}</strong>
-              </p>
-              <button>Add to cart</button>
+              <div className="product-info">
+                <Link to={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </Link>
+                <p>
+                  <strong>${product.price}</strong>
+                </p>
+                <button>Add to cart</button>
+              </div>
             </div>
-          </div>
-        ))}
+          ))
+        )}
       </div>
     </div>
   );
